3a041e0fdffd79e83bca09b41b49ac2442809830
Henrique Lira <henriquelirabilla@outlook.com.br>
2023-09-19 15:28:29 -0300

    Correções e ajustes da view , e configurando botão de upload

--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -21,7 +21,8 @@ namespace DevIO.App.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        //public IFormFile? ImagemUpload { get; set; }
+        [DisplayName("Imagem do Produto")]
+        public IFormFile? ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
