aeb59828ef1e377a21ee91a25c0d4245c7f2888f
Henrique Lira <henriquelirabilla@outlook.com.br>
2024-02-03 14:11:52 -0300

    Implementação da consulta via cep

--- src/DevIO.App/wwwroot/js/site.js
+++ src/DevIO.App/wwwroot/js/site.js
@@ -39,3 +39,63 @@
     })
 
 }
+
+function BuscaCep() {
+    $(document).ready(function () {
+
+        //Limpa valores do formulário de cep
+        $("#EnderecoLogradouro").val("")
+        $("#EnderecoBairro").val("")
+        $("#EnderecoCidade").val("")
+        $("#EnderecoEstado").val("")
+
+        //Quando o campo cep perde o foco
+        $("#EnderecoCep").blur(function () {
+
+            //Nova variável "cep" somente com dígitos
+            var cep = $(this).val().replace(/\D/g, '')
+
+            //Verifica se o campo cep possui valor atrelado
+            if (cep != "") {
+
+                //Expressão regular para validar o CEP
+                var validacep = /^[0-9]{8}$/;
+
+                //valida o formato do cep
+                if (validacep.test(cep)) {
+
+                    //Preencher os campos com '...' enquanto consulta webservice
+                    $("#EnderecoLogradouro").val("...")
+                    $("#EnderecoBairro").val("...")
+                    $("#EnderecoCidade").val("...")
+                    $("#EnderecoEstado").val("...")
+
+                    //Consulta o webservice viacep.com.br
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            if (!("erro" in dados)) {
+
+                                //Atualiza os campos com os valores de consulta
+                                $("#EnderecoLogradouro").val(dados.logradouro)
+                                $("#EnderecoBairro").val(dados.bairro)
+                                $("#EnderecoCidade").val(dados.localidade)
+                                $("#EnderecoEstado").val(dados.uf)
+                            } else {
+                                limpa_formulario_cep()
+                                alert("CEP não encontrado.")
+                            }
+                        })
+
+                } else {
+                    //Cep inválido
+                    limpa_formulario_cep()
+                    alert("Formatado de cep inválido.")
+                }
+            } else {
+                //Cep sem valor, limpa formulário
+                limpa_formulario_cep();
+            }
+        })
+
+    })
+}
