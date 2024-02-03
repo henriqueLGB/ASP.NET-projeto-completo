aafc810b772f8dc8a37d7b4657d7ebe3387920ab
Henrique Lira <henriquelirabilla@outlook.com.br>
2024-02-03 15:38:23 -0300

    Começando a validação pela regra de negócio.

---
+++ src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using DevIO.Business.Models;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach(var error in  validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            //Propaga o esse erro até a camada de apresentação
+        }
+
+        protected bool ExecutarValidacao<TV,TE>(TV validacao, TE entidade) where TV: AbstractValidator<TE> where TE: Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            //Validar o estado da entidade
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
